8696d18989bcdc99a8e2dbb64a53c7b059d5ce2f
Angel Lugo Tehuitzil <termopit@hotmail.com>
2023-12-06 22:37:25 -0600

    Layout first stage

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+
+    return (
+        <div className="flex bg-gradient-to-t from-white to-myBlue1 border justify-between md:justify-around">
+            <h1 className="text-xl font-bold p-2 m-1">Bishopric Agenda</h1>
+            <button className="text-2xl border bg-white rounded p-2 m-1">⚙️</button>
+        </div>
+    );
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,12 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        "myBlue1": "#ADD7F6",
+        "myBlue2": "#87BFFF",
+        "myBlue3": "#3F8EFC",
+        "myBlue4": "#2667FF",
+        "myBlue5": "#3B28CC",
       },
     },
   },
