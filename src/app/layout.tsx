4e97afa71b7780e19965935f5cb9d728e5cea588
Angel Lugo Tehuitzil <termopit@hotmail.com>
2023-12-07 22:47:06 -0600

    Building add task page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 
-import Header from './components/header'
+import Header from './components/Header'
 import Footer from './components/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -21,7 +21,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Header />
-        <main>{children}</main>
+        <main className='max-w-2xl mx-auto'>{children}</main>
         <Footer />
       </body>
     </html>
