629c551d197cab2b640404b3b1c5b3fb916e1537
Angel Lugo Tehuitzil <termopit@hotmail.com>
2023-12-09 00:06:10 -0600

    Building Add task

--- src/app/new-task/page.tsx
+++ src/app/new-task/page.tsx
@@ -1,6 +1,22 @@
+"use client";
+
 export default function NewTask() {
+
+    function selection(selection : any) {
+        console.log(selection);
+    }
     
     return(
-        <p className="p-5 mx-auto my-5">Within new-task page</p>
+        <form className="flex flex-col overflow-hidden fixed top-15 w-full p-6 border" action="">
+            <legend className="text-2xl font-bold">New Task</legend>
+            <label className="mt-6" htmlFor="test">Type</label>
+            <select disabled={true} onChange={(e) => {selection(e.target.value)} } className="border mt-2" name="test" id="test">
+                <option value="One">One</option>
+                <option value="Two">Two</option>
+                <option value="Three">Three</option>
+                <option value="Four">Four</option>
+                <option value="Five">Five</option>
+            </select>
+        </form>
     );
 }
