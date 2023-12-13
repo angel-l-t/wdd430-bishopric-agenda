4a42cd742905dc4ea140b573edb85deae002aa67
Angel Lugo Tehuitzil <termopit@hotmail.com>
2023-12-13 15:11:33 -0600

    Finished tasks component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 
 import Header from './components/Header'
-import Footer from './components/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -21,8 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Header />
-        <main className='max-w-2xl mx-auto my-16'>{children}</main>
-        
+        <main className='w-full my-16'>{children}</main>        
       </body>
     </html>
   )
--- src/app/new-task/page.tsx
+++ src/app/new-task/page.tsx
@@ -1,9 +1,14 @@
 import NewTaskForm from "@/app/components/NewTaskForm"
+import Footer from "../components/Footer";
+
 import { createTask } from "@/app/utils/new-task";
 
 export default function NewTask() {
         
     return(
-        <NewTaskForm createTask={createTask}/>
+        <>
+            <NewTaskForm createTask={createTask}/>
+            <Footer />
+        </>
     );
 }
